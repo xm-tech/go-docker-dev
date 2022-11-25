f8e9e9c0e04a40568c8e304c6cabefc3439e20bf
xm-tech <maxm09@163.com>
2022-11-25 23:11:09 +0800

    test daemon

--- main.go
+++ main.go
@@ -1,9 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"os/signal"
+)
 
 func main() {
-	fmt.Println("hello golang")
-	fmt.Println("hello docker")
-	fmt.Println("hello golang & docker")
+	go sigalHandler()
+	go func() {
+		fmt.Println("hello golang")
+		fmt.Println("hello docker")
+		fmt.Println("hello golang & docker")
+		// time.Sleep(time.Minute * 5)
+	}()
+	select {}
+}
+
+func sigalHandler() {
+	ch := make(chan os.Signal, 1)
+	signal.Notify(ch, os.Interrupt)
+	for {
+		<-ch
+		fmt.Println("os Interrupt")
+		os.Exit(0)
+	}
 }
